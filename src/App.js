7c05f9d70d603dfb9fad76f72e46602210bc4b32
Chimoha31 <mokochii1108@gmail.com>
2022-04-12 11:44:13 -0700

    test

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ const App = () => {
       <div className="sidebar">
         <ul className="nav">
           <li>
-            <CustomLink to="/">Home</CustomLink>
+            <CustomLink to="/">Home(Test)</CustomLink>
           </li>
           <li>
             <CustomLink to="/login">Login</CustomLink>
